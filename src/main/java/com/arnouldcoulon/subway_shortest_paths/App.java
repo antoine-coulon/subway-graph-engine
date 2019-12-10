716f981642eacd1450f79201475d334285979d50
YoannArnould <43949560+YoannArnould@users.noreply.github.com>
2019-12-10 21:47:04 +0100

    init maven project

---
+++ src/main/java/com/arnouldcoulon/subway_shortest_paths/App.java
@@ -0,0 +1,13 @@
+package com.arnouldcoulon.subway_shortest_paths;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
