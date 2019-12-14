7f28083de7357246e2486203a533c4732baa360e
YoannArnould <43949560+YoannArnould@users.noreply.github.com>
2019-12-14 14:31:35 +0100

    add plus long chemin

--- src/main/java/com/arnouldcoulon/subway_shortest_paths/App.java
+++ src/main/java/com/arnouldcoulon/subway_shortest_paths/App.java
@@ -17,6 +17,8 @@ import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.SimpleGraph;
 import org.jgrapht.alg.shortestpath.BellmanFordShortestPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.alg.shortestpath.KShortestSimplePaths;
+import org.jgrapht.alg.shortestpath.AllDirectedPaths;
 
 import com.arnouldcoulon.subway.Correspondance;
 import com.arnouldcoulon.subway.JsonDataReader;
@@ -81,12 +83,32 @@ public class App
         	
         }
      
+        //AllDirectedPaths inspectorPath = new AllDirectedPaths(graphSubway);
+        //List<GraphPath> paths = inspectorPath.getAllPaths(stations.get(0), stations.get(stations.size()-1), false, Integer.MAX_VALUE);
+       //KShortestPaths<Station, DefaultEdge> pathInspector = new KShortestPaths<Station, DefaultEdge>(graphSubway,Integer.MAX_VALUE, Integer.MAX_VALUE);
         //GraphPath shortestPath = BellmanFordShortestPath.findPathBetween(graphSubway,stations.get(0), stations.get(stations.size()-1));
         //shortestPath.get
        // Iterator<GraphPath<Station,DefaultEdge>> it_path = new EppsteinShortestPathIterator(graphSubway,stations.get(0), stations.get(stations.size()-1));
         
         //System.out.println(subwayDatas.toString());
 
+        KShortestSimplePaths shortestPathsFinder = new KShortestSimplePaths(graphSubway);
+        List<GraphPath<Station,DefaultEdge>> shortestPaths = shortestPathsFinder.getPaths(stations.get(0), stations.get(stations.size()-1),200);
+        
+        List<Station>  shortPath = shortestPaths.get(0).getVertexList();
+        GraphPath longestShortPath = shortestPaths.get(shortestPaths.size()-1);
+        List<Station> pathFound = longestShortPath.getVertexList();
+        
+        System.out.println(" TRAJET PLUS COURT DE "+ stations.get(0).getNom() +" A "+ stations.get(stations.size()-1).getNom()+"\n");
+        for(Station station : shortPath) {
+        	System.out.println(station.getNom());
+        }
+        
+        
+        System.out.println(" TRAJET DE "+ stations.get(0).getNom() +" A "+ stations.get(stations.size()-1).getNom()+"\n");
+        for(Station station : pathFound) {
+        	System.out.println(station.getNom());
+        }
 
 }
 }
