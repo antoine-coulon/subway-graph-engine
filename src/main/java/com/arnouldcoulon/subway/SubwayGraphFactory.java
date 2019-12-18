8d5af7e76651cb78c6b1ace57ed8b99dcb16a08e
YoannArnould <43949560+YoannArnould@users.noreply.github.com>
2019-12-18 10:23:30 +0100

    add subwayGraphFactory + clean main

---
+++ src/main/java/com/arnouldcoulon/subway/SubwayGraphFactory.java
@@ -0,0 +1,71 @@
+package com.arnouldcoulon.subway;import java.util.List;
+
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleGraph;
+
+public class SubwayGraphFactory {
+	
+	private static int SIMPLE_GRAPH = 0;
+	private static int WEIGHTED_GRAPH = 1;
+	
+	
+	public static Graph<Station, DefaultEdge> createSubwayGraph(){
+		
+		JsonDataReader subwayDatas = new JsonDataReader();
+        
+        //Generation du graph simple
+        
+        Graph<Station, DefaultEdge> graphSubway = new SimpleGraph<>(DefaultEdge.class);
+        
+        //Creation des vertex
+        List<Station> stations = subwayDatas.getListStations();
+        for(Station station : stations) {
+        	graphSubway.addVertex(station);
+        }
+        
+        //Creation des edges
+        
+        List<Ligne> lignes = subwayDatas.getLignes();
+        for(Ligne ligne : lignes) {
+        	List<List<Station>> routes =ligne.getRoutes();
+        	for(List<Station> route : routes) {
+        		Station lastStation = null;
+        		for(Station station :route) {
+        			if(lastStation!=null)
+        				graphSubway.addEdge(lastStation, station);
+        			lastStation = station;
+        		}
+        		
+        	}
+        }
+        
+        
+        //Creation des edges : correspondances
+       
+        for(Station station : stations) {
+        	
+        	 //Si cette station a des correspondances
+            if(Correspondance.hasCorrespondances(station)) {
+            	// On récupère une liste de station qui sont les correspondances de la station donnée 
+            	List<Station> ls = Correspondance.getStationCorrespondances(station);
+            	for(Station correspStation: ls) {
+            		// Ajout d'un edge entre cette station et la correspondance par exemple 
+            		graphSubway.addEdge(station,correspStation);
+            	}
+            }
+        	
+        }
+        
+        System.out.println( "Graph pret !" );
+        
+		return graphSubway;
+		
+	}
+	
+	public static Graph<Station, DefaultEdge> createSubwayWeightedGraph(){
+		return null;
+		
+	}	
+
+}
--- src/main/java/com/arnouldcoulon/subway_shortest_paths/App.java
+++ src/main/java/com/arnouldcoulon/subway_shortest_paths/App.java
@@ -10,6 +10,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.jgrapht.Graph;
 import org.jgrapht.GraphPath;
@@ -24,6 +25,7 @@ import com.arnouldcoulon.subway.Correspondance;
 import com.arnouldcoulon.subway.JsonDataReader;
 import com.arnouldcoulon.subway.Ligne;
 import com.arnouldcoulon.subway.Station;
+import com.arnouldcoulon.subway.SubwayGraphFactory;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -36,52 +38,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        
-        JsonDataReader subwayDatas = new JsonDataReader();
-        
-        //Generation du graph simple
-        
-        Graph<Station, DefaultEdge> graphSubway = new SimpleGraph<>(DefaultEdge.class);
-        
-        //Creation des vertex
-        List<Station> stations = subwayDatas.getListStations();
-        for(Station station : stations) {
-        	graphSubway.addVertex(station);
-        }
-        
-        //Creation des edges
-        
-        List<Ligne> lignes = subwayDatas.getLignes();
-        for(Ligne ligne : lignes) {
-        	List<List<Station>> routes =ligne.getRoutes();
-        	for(List<Station> route : routes) {
-        		Station lastStation = null;
-        		for(Station station :route) {
-        			if(lastStation!=null)
-        				graphSubway.addEdge(lastStation, station);
-        			lastStation = station;
-        		}
-        		
-        	}
-        }
-        
-        
-        //Creation des edges : correspondances
-        
-        for(Station station : stations) {
-        	
-        	 //Si cette station a des correspondances
-            if(Correspondance.hasCorrespondances(station)) {
-            	// On récupère une liste de station qui sont les correspondances de la station donnée 
-            	List<Station> ls = Correspondance.getStationCorrespondances(station);
-            	for(Station correspStation: ls) {
-            		// Ajout d'un edge entre cette station et la correspondance par exemple 
-            		graphSubway.addEdge(station,correspStation);
-            	}
-            }
-        	
-        }
+    	
+    	Graph<Station, DefaultEdge> graphSubway = SubwayGraphFactory.createSubwayGraph();
      
         //AllDirectedPaths inspectorPath = new AllDirectedPaths(graphSubway);
         //List<GraphPath> paths = inspectorPath.getAllPaths(stations.get(0), stations.get(stations.size()-1), false, Integer.MAX_VALUE);
@@ -92,8 +50,11 @@ public class App
         
         //System.out.println(subwayDatas.toString());
 
+    	List<Station> stations = new ArrayList<>(graphSubway.vertexSet());
+        
+        
         KShortestSimplePaths shortestPathsFinder = new KShortestSimplePaths(graphSubway);
-        List<GraphPath<Station,DefaultEdge>> shortestPaths = shortestPathsFinder.getPaths(stations.get(0), stations.get(stations.size()-1),200);
+        List<GraphPath<Station,DefaultEdge>> shortestPaths = shortestPathsFinder.getPaths(stations.get(0), stations.get(stations.size()-1),300);
         
         List<Station>  shortPath = shortestPaths.get(0).getVertexList();
         GraphPath longestShortPath = shortestPaths.get(shortestPaths.size()-1);
@@ -108,6 +69,7 @@ public class App
         System.out.println(" TRAJET DE "+ stations.get(0).getNom() +" A "+ stations.get(stations.size()-1).getNom()+"\n");
         for(Station station : pathFound) {
         	System.out.println(station.getNom());
+        	
         }
 
 }
